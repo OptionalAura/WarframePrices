002c1bccb06ca523de26e67bdf171f8d21efbab4
Daniel Allen <optionalaura@gmail.com>
2022-01-24 23:30:59 -0500

    Added copyright notices

--- src/WarframeMarket.java
+++ src/WarframeMarket.java
@@ -1,13 +1,18 @@
-import org.json.JSONObject;
-
-import javax.net.ssl.HttpsURLConnection;
-import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.http.HttpClient;
+/*
+ * Copyright 2022 Daniel Allen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 public class WarframeMarket {
 
